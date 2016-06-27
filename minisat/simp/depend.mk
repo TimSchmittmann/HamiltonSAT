b61bdf6466ddc235a2cc32a1aeaa0e7994b08445
root <root@ubuntu.(none)>
2016-06-27 03:06:51 +0200

    Add minisat to root dir

---
+++ minisat/simp/depend.mk
@@ -0,0 +1,33 @@
+/home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/Main.o /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/Main.or /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/Main.od /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/Main.op: /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/Main.cc \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/utils/System.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/IntTypes.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/utils/ParseUtils.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/utils/Options.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Vec.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/XAlloc.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/core/Dimacs.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/core/SolverTypes.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Alg.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Map.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Alloc.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/SimpSolver.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Queue.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/core/Solver.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Heap.h
+/home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/SimpSolver.o /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/SimpSolver.or /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/SimpSolver.od /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/SimpSolver.op: \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/SimpSolver.cc \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Sort.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Vec.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/IntTypes.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/XAlloc.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/simp/SimpSolver.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Queue.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/core/Solver.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Heap.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Alg.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/utils/Options.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/utils/ParseUtils.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/core/SolverTypes.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Map.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/mtl/Alloc.h \
+ /home/furio/HamiltonSAT/solvers/HamiltonSAT/minisat/utils/System.h
