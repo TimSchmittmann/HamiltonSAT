82b6307ad2b79fdc749767289b155701fbd45e1f
root <root@ubuntu.(none)>
2016-06-29 13:23:50 +0200

    Whatever I did...it worked

--- verify.sh
+++ verify.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 FILES=graphs/*;
-SOLVER=solvers/HamiltonSAT
+SOLVER=solvers/solosat
 echo "" > test.txt
 for f in $FILES;
 do 
 	echo $f >> $SOLVER/test.txt;
-	./$SOLVER/HamiltonSAT.exe $f | ./hc-verifier -v -w $f >> $SOLVER/test.txt 
+	./$SOLVER/solosat.exe $f  | ./hc-verifier -v -w $f >> $SOLVER/test.txt
 done;
 
